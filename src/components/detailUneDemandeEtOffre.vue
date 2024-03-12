c1e99e257a3e9eb77e90ffead425fb1c83c84ab3
Mariama Diaby <diabymariama414@gmail.com>
2024-03-12 16:42:15 -0700

    construction html de structure html fiche detail demande et offre de stage fait

---
+++ src/components/detailUneDemandeEtOffre.vue
@@ -0,0 +1,36 @@
+<template>
+    <main>
+        <div class="containerTitresPrincipaux">
+        </div>
+        <div class="containerModeConsultation">
+        </div>
+        <div class="containerInformationsDetaillés">
+            <h2></h2>
+            <p></p>
+            <div class="containerInformations">
+                <div class="containerInfos">
+                    <div class="blocStyle"></div>
+                    <h3></h3>
+                    <span></span>
+                </div>
+            </div>
+            <div class="containerInformationSpecifique">
+                <h2></h2>
+                <div class="containerInfos">
+                    <div class="blocStyle"></div>
+                    <h3></h3>
+                    <span></span>
+                </div>
+            </div>
+            <div class="containerInformationsSuppl">
+                <h2></h2>
+                <p></p>
+            </div>
+        </div>
+    </main>
+</template>
+  
+<script setup>
+  
+</script>
+  
