ae12022ed91499e8d1b437831004b4a1a469995a
Megan Martel <115752040+meganmartel@users.noreply.github.com>
2024-03-12 20:03:22 -0400

    validation formulaire champs vide

--- src/views/Identification.vue
+++ src/views/Identification.vue
@@ -7,18 +7,20 @@
       <div class="w-1/2">
         <img src="/src/assets/img/home-students.png" alt="home-students" class="w-full h-auto">
       </div>
-      <div class="w-1/2 bg-gray-200">
+      <div class="w-1/2 bg-gray-200 flex items-center">
         <div class="p-20">
           <h2 class="text-7xl mb-5">Identification</h2>
           <p class="w-3/4 mb-24">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet maiores neque eum quia! Quisquam, nisi voluptate.</p>
           <form @submit.prevent="acceder">
-            <div>
-              <input type="text" id="nomEtPrenom" v-model="nomEtPrenom" placeholder="Votre nom et prénom" required class="border-teal-500 border-solid border-2 p-3">
+            <div class="flex flex-col mb-10">
+              <input type="text" id="nomEtPrenom" v-model="nomEtPrenom" placeholder="Votre nom et prénom" class="border-teal-500 border-solid border-2 p-3 w-3/4">
+              <span v-if="!nomEtPrenomValid" class="text-red-500">Veuillez entrer votre nom et prénom.</span>
             </div>
-            <div>
-              <input type="email" id="courriel" v-model="courriel" placeholder="Votre courriel" required>
+            <div class="flex flex-col mb-10">
+              <input type="email" id="courriel" v-model="courriel" placeholder="Votre courriel" class="border-teal-500 border-solid border-2 p-3 w-3/4">
+              <span v-if="!courrielValid" class="text-red-500">Veuillez entrer votre courriel.</span>
             </div>
-            <button type="submit">Accéder maintenant</button>
+            <button type="submit" class="bg-teal-500 border rounded p-5 text-white">Accéder maintenant</button>
           </form>
         </div>
       </div>
@@ -26,7 +28,30 @@
   </main>
 </template>
 
-  <script setup>
-  
-  </script>
+<script setup>
+import { ref } from 'vue';
+
+const nomEtPrenom = ref('');
+const courriel = ref('');
+const nomEtPrenomValid = ref(true);
+const courrielValid = ref(true);
+
+const acceder = () => {
+  if (!nomEtPrenom.value) {
+    nomEtPrenomValid.value = false;
+  } else {
+    nomEtPrenomValid.value = true;
+  }
+
+  if (!courriel.value) {
+    courrielValid.value = false;
+  } else {
+    courrielValid.value = true;
+  }
+
+  if (!nomEtPrenom.value || !courriel.value) {
+    return;
+  }
+}
+</script>
   
