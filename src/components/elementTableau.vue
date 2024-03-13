fb80054f35881ebfbb67d1b60ee79767a3395447
karen102_14 <karen102_14@hotmail.com>
2024-03-12 22:23:19 -0400

    Ajout de balises et de styles de base pour le composant de liste de tableau

---
+++ src/components/elementTableau.vue
@@ -0,0 +1,43 @@
+<template>
+    <td class="border-l-4 border-yellow-600">
+        <div class="ml-4 flex flex-row">
+            <div class="p-3 bg-yellow-600/[.5] rounded-lg">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-mortarboard-fill text-yellow-800" viewBox="0 0 16 16">
+                    <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917z"/>
+                    <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466z"/>
+                </svg>
+            </div>
+            <div class="ml-3 flex flex-col">
+                <p>Intégrateur Web</p>
+                <p>Jean-Sébastien tremblay</p>
+            </div>
+        </div>
+    </td>
+    <td>
+        <p>Nouvelles technologies</p>
+    </td>
+    <td>
+        <p>Mauricie</p>
+    </td>
+    <td>
+        <p>2022-03-02</p>
+    </td>
+    <td class="flex flex-row">
+        <button class="py-2 px-3 bg-green-200/[.6] text-green-500 font-medium rounded-lg">Accepter</button>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill text-teal-500" viewBox="0 0 16 16">
+            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
+            <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
+        </svg>  
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square text-yellow-600" viewBox="0 0 16 16">
+            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill text-red-800" viewBox="0 0 16 16">
+            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+        </svg>
+    </td>
+</template>
+
+<script setup>
+
+</script>
--- src/views/TableauDeBord.vue
+++ src/views/TableauDeBord.vue
@@ -1,7 +1,22 @@
 <template>
     <h1>Tableau de bord</h1>
+    <table>
+        <tr>
+            <th class="text-left">Profil</th>
+<!--             <th class="text-left">Poste</th> -->
+            <th class="text-left">Secteur d'activité</th>
+<!--             <th class="text-left">Établissement</th>
+            <th class="text-left">Ville</th> -->
+            <th class="text-left">Région</th>
+            <th class="text-left">Date d'inscription</th>
+            <th class="text-left">Actions</th>
+        </tr>
+        <tr>
+            <ElementTableau />
+        </tr>
+    </table>
 </template>
 
 <script setup>
-
+import ElementTableau from '../components/elementTableau.vue'
 </script>
