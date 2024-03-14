1004fe060b2dffecf8340c8a8b3a37d5b9461918
rox perron <roxanneperron11@gmail.com>
2024-03-13 21:50:19 -0400

    Continuer les validations des champs pour le formulaireDO et ajouter les champs requis que pour les offres.

--- src/views/FormulaireDO.vue
+++ src/views/FormulaireDO.vue
@@ -6,7 +6,7 @@
 
 
     <!-- Formulaire  -->
-    <form class="max-w-2xl mx-auto mt-10" @submit.prevent="validationFormulaire">
+    <form class="max-w-2xl mx-auto mt-10" @submit.prevent="soumettreFormulaireDO">
 
       <div class="max-w-2xl mx-auto mt-10">
         <div>
@@ -15,9 +15,17 @@
               alt="Image de disquette">Sauvegarder</button>
         </div>
 
-        <label for="titre" class="block mb-2">Titre</label>
+        <label for="titre" class="block mb-2">Titre: </label>
         <input id="titre" v-model="titre" type="text" class="border border-gray-300 p-2 w-full mb-6">
-        <span v-if="erreurs.titre" class="text-red-500">{{ erreurs.titre}}</span>
+        <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
+
+        <label for="entreprises" class="block mb-2">Entreprise: </label>
+        <Select id="entreprises" v-model="entreprises" type="text" class="border border-gray-300 p-2 w-full mb-6">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+        </select>
+        <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
       </div>
 
 
@@ -25,103 +33,114 @@
       <div>
         <label for="nomComplet" class="block mb-2">Nom et prénom</label>
         <input id="nomComplet" v-model="nomComplet" class="border border-gray-300 p-2 w-full mb-6"></input>
-        <span v-if="erreurs.nomComplet" class="text-red-500">{{ erreurs.nomComplet }}</span>
+        <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
+
 
         <label for="presentation" class="block mb-2">Présensention</label>
         <textarea id="presentation" v-model="presentation" class="border border-gray-300 p-2 w-full mb-6"></textarea>
-        <span v-if="erreurs.presentation" class="text-red-500">{{ erreurs.presentation }}</span>
+        <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
+
 
         <div>
           <label for="programme" class="block mb-2">Programme de formation</label>
           <input id="programme" v-model="programme" type="text" class="border border-gray-300 p-2 w-full mb-6">
-          <span v-if="erreurs.programme" class="text-red-500">{{ erreurs.programme }}</span>
+          <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
+
+
+          <label for="exigences" class="block mb-2">Exigences</label>
+          <textarea id="exigences" v-model="exigences" class="border border-gray-300 p-2 w-full mb-6"></textarea>
+          <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
+
 
           <label for="etablissement" class="block mb-2">Établissement scolaire</label>
           <input id="etablissement" v-model="etablissement" type="text" class="border border-gray-300 p-2 w-full mb-6">
-          <span v-if="erreurs.etablissement" class="text-red-500">{{ erreurs.etablissement }}</span>
+          <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
+
 
           <label for="secteurActivite" class="block mb-2">Secteur d'activité</label>
           <Select id="secteurActivite" v-model="secteurActivite" type="text"
             class="border border-gray-300 p-2 w-full mb-6">
-            <option>Sélectionner une option</option>
-            <option>Technologie de l'information</option>
-            <option>Santé</option>
-            <option>Finance</option>
-            <option>Éducation</option>
-            <option>Transport et logistique</option>
-            <option>Immoblier</option>
-            <option>Tourisme</option>
-            <option>Manufacture</option>
-            <option>Énergie</option>
-            <option>Commerce au détail</option>
+            <option value="option1"></option>
+            <option value="option2">Technologie de l'information</option>
+            <option value="option3">Santé</option>
+            <option value="option4">Finance</option>
+            <option value="option5">Éducation</option>
+            <option value="option6">Transport et logistique</option>
+            <option value="option7">Immoblier</option>
+            <option value="option8">Tourisme</option>
+            <option value="option9">Manufacture</option>
+            <option value="option10">Énergie</option>
+            <option value="option11">Commerce au détail</option>
           </select>
+          <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
         </div>
 
         <label for="ville" class="block mb-2">Ville</label>
         <input id="ville" v-model="ville" class="border border-gray-300 p-2 w-full mb-6"></input>
-        <span v-if="erreurs.ville" class="text-red-500">{{ erreurs.ville }}</span>
+        <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
+
 
         <label for="region" class="block mb-2">Région</label>
         <Select id="region" v-model="region" type="text" class="border border-gray-300 p-2 w-full mb-6">
-          <option>Sélectionner une option</option>
-          <option>Québec</option>
-          <option>Trois-Rivière</option>
-          <option>Montréal</option>
-          <option>Sherbrooke</option>
-          <option>Gatineau</option>
+          <option value="option1"></option>
+          <option value="option2">Québec</option>
+          <option value="option3">Trois-Rivière</option>
+          <option value="option4">Montréal</option>
+          <option value="option5">Sherbrooke</option>
+          <option value="option6">Gatineau</option>
         </select>
+        <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
       </div>
 
       <label for="competences" class="block mb-2">Compétences</label>
       <textarea id="competences" v-model="competences" class="border border-gray-300 p-2 w-full mb-6"></textarea>
-      <span v-if="erreurs.competences" class="text-red-500">{{ erreurs.competences }}</span>
+      <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
+
 
       <h2 class="p-2 w-full mb-6">Information sur le stage</h2>
       <label for="typeStage" class="block mb-2">Type de stage</label>
       <select id="typeStage" v-model="typeStage" class="border border-gray-300 p-2 w-full mb-6">
-        <option value="option1">Sélectionner une option</option>
+        <option value="option1"></option>
         <option value="option2">Temps plein</option>
         <option value="option3">Temps partiel</option>
       </select>
+      <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
+
 
       <label for="dateDebut" class="block mb-2">Date de début</label>
       <input id="dateDebut" v-model="dateDebut" type="date" class="border border-gray-300 p-2 w-full mb-6">
-      <span v-if="erreurs.dateDebut" class="text-red-500">{{ erreurs.dateDebut }}</span>
+      <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
+
 
       <label for="heuresSemaine" class="block mb-2">Nombre d'heures par semaine</label>
       <input id="heuresSemaine" v-model="heuresSemaine" type="number" class="border border-gray-300 p-2 w-full mb-6">
-      <span v-if="erreurs.heuresSemaine" class="text-red-500">{{ erreurs.heuresSemaine}}</span>
+      <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
+
 
       <label for="dateFin" class="block mb-2">Date de fin</label>
       <input id="dateFin" v-model="dateFin" type="date" class="border border-gray-300 p-2 w-full mb-6">
-      <span v-if="erreurs.dateFin" class="text-red-500">{{ erreurs.dateFin}}</span>
+      <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
+
 
       <h2 class="p-2 w-full mb-2">Rémumération</h2>
-      <input id="aDiscuter" type="checkbox" class="mr-2">
+      <input id="aDiscuter" v-model="aDiscuter" type="checkbox" class="mr-2">
       <label for="aDiscuter">À discuter</label><br>
-      <input id="remunere" type="checkbox" class="mr-2">
+      <input id="remunere" v-model="remunere" type="checkbox" class="mr-2">
       <label for="remunere">Stage rémunéré</label><br>
-      <input id="nonRemunere" type="checkbox" class="mr-2">
+      <input id="nonRemunere" v-model="nonRemunere" type="checkbox" class="mr-2">
       <label for="nonRemunere">Stage non rémunéré</label><br>
+      <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
+
 
       <label for="infoSupplementaire" class="p-2 w-full mb-2">Information supplémentaire</label>
       <textarea id="infoSupplementaire" v-model="infoSupplementaire"
         class="border border-gray-300 p-2 w-full mb-6"></textarea>
-
-
-
-
+        <span v-if="!formulaireValide" class="text-red-700">{{ erreurFormulaire }}</span>
 
 
       <input
         class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
         aria-describedby="file_input_help" id="file_input" type="file">
-
-
-
-
-
-
 
       <div>
         <button class="bg-blue-500 text-white px-4 py-2 m-1 rounded">Annuler</button>
@@ -134,8 +153,8 @@
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue'
-// Création des variables réactions 
+import { ref } from 'vue'
+// Création des variables réactives 
 const titre = ref('');
 const nomComplet = ref('');
 const presentation = ref('');
@@ -153,87 +172,68 @@ const aDiscuter = ref('');
 const remunere = ref('');
 const nonRemunere = ref('');
 const infoSupplementaire = ref('');
+const formulaireValide = ref(true);
+const erreurFormulaire = ref('')
 
-//Création des variables pour gérer les erreurs lorsque les champs restent vides
-const erreursChampsVides = reactive({
-  titre: '',
-  nomComplet: '',
-  presentation: '',
-  programme: '',
-  etablissement: '',
-  secteurActivite: '',
-  ville: '',
-  region: '',
-  competences: '',
-  typeStage: '',
-  dateDebut: '',
-  heuresSemaine: '',
-  dateFin: '',
-/*   aDiscuter: '',
-  remunere: '',
-  nonRemunere: '', */
-  infoSupplementaire: '',
-})
 
-// Fonction pour valider le formulaire 
-function validationFormulaire() {
-  const erreurs = [];
-  const regexLettresSeulement = /^[\p{L} -]+$/u;
 
-  if (!regexLettresSeulement.test(titre.value.trim())) {
-    erreursChampsVides.titre('Le champ "Titre" est obligatoire.');
-  }
-  if (!regexLettresSeulement.test(nomComplet.value.trim())) {
-    erreursChampsVides.nomComplet('Le champ "Nom et prénom" est obligatoire.');
-  }
-  if (presentation.value.trim() === '') {
-    erreursChampsVides.presentation('Le champ "Présentation" est obligatoire.');
-  }
-  if (!regexLettresSeulement.test(programme.value.trim())) {
-    erreursChampsVides.programme('Le champ "Programme de formation" est obligatoire.');
-  }
-  if (!regexLettresSeulement.test(etablissement.value.trim())) {
-    erreursChampsVides.etablissement('Le champ "Établissement scolaire" est obligatoire.');
-  }
- /*  if (secteurActivite.value.trim() === '') {
-    erreurs.secteurActivite.value('Le champ "Secteur d\'activité" est obligatoire.');
-  } */
-  if (!regexLettresSeulement.test(ville.value.trim())) {
-    erreursChampsVides.ville('Le champ "Ville" est obligatoire.');
-  }
-  /* if (region.value.trim() === '') {
-    erreurs.region.value('Le champ "Région" est obligatoire.');
-  } */
-  if (!regexLettresSeulement.test(competences.value.trim())) {
-    erreursChampsVides.competences('Le champ "Compétences" est obligatoire.');
-  }
-  if (typeStage.value.trim() === '') {
-    erreursChampsVides.typeStage('Le champ "Type de stage" est obligatoire.');
-  }
-  if (dateDebut.value.trim() === '') {
-    erreursChampsVides.dateDebut('Le champ "Date de début" est obligatoire.');
-  }
-  if (heuresSemaine.value.trim() === '') {
-    erreursChampsVides.heuresSemaine('Le champ "Nombre d\'heures par semaine" est obligatoire.');
-  }
-  if (dateFin.value.trim() === '') {
-    erreursChampsVides.dateFin('Le champ "Date de fin" est obligatoire.');
-  }
-/*   if (!aDiscuter.value && !remunere.value && !nonRemunere.value) {
-    erreurs.('Au moins un des champs "Rémunération" doit être sélectionné.');
-  } */
-  if (infoSupplementaire.value.trim() === '') {
-    erreursChampsVides.infoSupplementaire.value('Le champ "Information supplémentaire" est obligatoire.');
-  }
 
-  if (erreurs.length > 0) {
-    // Afficher les erreurs ou les traiter comme nécessaire
-    console.log('Erreurs de validation :', erreurs);
+//Fonction pour valider les champs du formulaire
+const soumettreFormulaireDO = () => {
+  if (
+    // Vérification des champs obligatoires
+    titre.value.trim() === '' ||
+    nomComplet.value.trim() === '' ||
+    presentation.value.trim() === '' ||
+    programme.value.trim() === '' ||
+    competences.value.trim() === '' ||
+    etablissement.value.trim() === '' ||
+    ville.value.trim() === '' ||
+    dateDebut.value === '' ||
+    heuresSemaine.value === '' ||
+    dateFin.value === '' ||
+    secteurActivite.value === 'option1' ||
+    region.value === 'option1'
+  ) {
+    formulaireValide.value = false;
+    erreurFormulaire.value = "Le champ est obligatoire.";
+  } else if (
+    // Vérification de la longueur des champs
+    titre.value.trim() !== '' && (titre.value.length < 5 || titre.value.length > 100) ||
+    nomComplet.value.trim() !== '' && (nomComplet.value.length < 5 || nomComplet.value.length > 100) ||
+    presentation.value.trim() !== '' && (presentation.value.length < 5 || presentation.value.length > 100) ||
+    programme.value.trim() !== '' && (programme.value.length < 5 || programme.value.length > 100) ||
+    competences.value.trim() !== '' && (competences.value.length < 5 || competences.value.length > 100) ||
+    etablissement.value.trim() !== '' && (etablissement.value.length < 5 || etablissement.value.length > 100) ||
+    ville.value.trim() !== '' && (ville.value.length < 5 || ville.value.length > 100) ||
+    competences.value.trim() !== '' && (competences.value.length < 5 || competences.value.length > 100) ||
+    !/^\d+$/.test(heuresSemaine.value)
+  ) {
+    formulaireValide.value = false;
+    erreurFormulaire.value = "Le champ doit avoir entre 5 et 100 caractères.";
+    if (!/^\d+$/.test(heuresSemaine.value)) {
+      erreurFormulaire.value = "Seuls les chiffres sont autorisés dans ce champs.";
+    }
+  } else if (
+    (aDiscuter.value && (remunere.value || nonRemunere.value)) ||
+    (remunere.value && (aDiscuter.value || nonRemunere.value)) ||
+    (nonRemunere.value && (aDiscuter.value || remunere.value))
+  ) {
+    formulaireValide.value = false;
+    erreurFormulaire.value = "Veuillez sélectionner une seule option de rémunération.";
   } else {
-    // Envoyer le formulaire ou effectuer d'autres actions si la validation réussit
-    console.log('Formulaire valide. Traitement en cours...');
+    formulaireValide.value = true;
+    erreurFormulaire.value = '';
   }
 }
+
+
+
+
+
+
+
+
 
 
 
