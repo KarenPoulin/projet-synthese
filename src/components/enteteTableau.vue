5738db9b133fca751592bb58d677871a903b8f1b
karen102_14 <karen102_14@hotmail.com>
2024-03-13 22:26:36 -0400

    CSS statique des éléments et de l'entête terminé. presque terminé le CSS dynamique

---
+++ src/components/enteteTableau.vue
@@ -0,0 +1,33 @@
+<template>
+    <tr>
+        <th>
+            <p class="mr-6 pb-1 text-left text-neutral-800 border-neutral-800 border-b-4">Profil</p>
+        </th>
+<!--             <th class="text-left">Poste</th> -->
+        <th v-if="isDemandes && isTableauDeBord">
+            <p class="mr-6 pb-1 text-left text-neutral-800 border-neutral-800 border-b-4">Établissement</p>
+        </th>
+        <th v-if="!isDemandes && isTableauDeBord">
+            <p class="mr-6 pb-1 text-left text-neutral-800 border-neutral-800 border-b-4">Ville</p>
+        </th>
+        <th v-if="!isTableauDeBord">
+            <p class="mr-6 pb-1 text-left text-neutral-800 border-neutral-800 border-b-4">Secteur d'activité</p>
+        </th>
+        <th v-if="!isTableauDeBord">
+            <p class="mr-6 pb-1 text-left text-neutral-800 border-neutral-800 border-b-4">Région</p>
+        </th>
+        <th>
+            <p class="mr-6 pb-1 text-left text-neutral-800 border-neutral-800 border-b-4">Date d'inscription</p>
+        </th>
+        <th>
+            <p class="pb-1 text-left text-neutral-800 border-neutral-800 border-b-4">Actions</p>
+        </th>
+    </tr>
+</template>
+
+<script setup>
+        const props = defineProps({
+        isDemandes: Boolean,
+        isTableauDeBord: Boolean,
+    })
+</script>
--- src/views/TableauDeBord.vue
+++ src/views/TableauDeBord.vue
@@ -1,22 +1,25 @@
 <template>
-    <h1>Tableau de bord</h1>
-    <table>
-        <tr>
-            <th class="text-left">Profil</th>
-<!--             <th class="text-left">Poste</th> -->
-            <th class="text-left">Secteur d'activité</th>
-<!--             <th class="text-left">Établissement</th>
-            <th class="text-left">Ville</th> -->
-            <th class="text-left">Région</th>
-            <th class="text-left">Date d'inscription</th>
-            <th class="text-left">Actions</th>
-        </tr>
-        <tr>
-            <ElementTableau />
-        </tr>
+    <table class="w-full mb-10">
+        <EnteteTableau :isDemandes="true" :isTableauDeBord="true" />
+        <ElementTableau :isDemandes="true" :isTableauDeBord="true" :isActive="true" />
+    </table>
+    <table class="w-full mb-10">
+        <EnteteTableau :isDemandes="false" :isTableauDeBord="true" />
+        <ElementTableau :isDemandes="false" :isTableauDeBord="true" :isActive="true"/>
+    </table>
+    <table class="w-full mb-10">
+        <EnteteTableau :isDemandes="true" :isTableauDeBord="false"  />
+        <ElementTableau :isDemandes="true" :isTableauDeBord="false" :isActive="true"/>
+        <ElementTableau :isDemandes="true" :isTableauDeBord="false" :isActive="false"/>
+    </table>
+    <table class="w-full mb-10">
+        <EnteteTableau :isDemandes="false" :isTableauDeBord="false" />
+        <ElementTableau :isDemandes="false" :isTableauDeBord="false" :isActive="true"/>
+        <ElementTableau :isDemandes="false" :isTableauDeBord="false" :isActive="false"/>
     </table>
 </template>
 
 <script setup>
 import ElementTableau from '../components/elementTableau.vue'
+import EnteteTableau from '../components/enteteTableau.vue'
 </script>
