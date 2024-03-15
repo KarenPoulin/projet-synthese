eeb3d451bff86745ae46d83d8416a60e97624034
Megan Martel <115752040+meganmartel@users.noreply.github.com>
2024-03-14 21:11:49 -0400

    début du responsive page identification

--- src/views/Identification.vue
+++ src/views/Identification.vue
@@ -1,26 +1,26 @@
 <template>
   <main>
     <div>
-      <h1><img src="/src/assets/img/logo.png" alt="logo" class="h-28"></h1>
+      <h1><img src="/src/assets/img/logo.png" alt="logo" class="h-24"></h1>
     </div>
-    <div class="flex">
-      <div class="w-1/2">
+    <div class="flex flex-col xl:flex-row">
+      <div class="xl:w-1/2">
         <img src="/src/assets/img/home-students.png" alt="home-students" class="w-full h-auto">
       </div>
-      <div class="w-1/2 bg-gray-200 flex items-center">
-        <div class="p-20">
-          <h2 class="text-7xl mb-5">Identification</h2>
-          <p class="w-3/4 mb-24">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet maiores neque eum quia! Quisquam, nisi voluptate.</p>
+      <div class="xl:w-1/2 bg-gray-200 flex">
+        <div class="p-10 xl:p-15 2xl:p-20 xl:items-center">
+          <h2 class="text-5xl lg:text-7xl mb-5">Identification</h2>
+          <p class="md:w-5/6 lg:w-3/4 mb-10 xl:mb-20">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet maiores neque eum quia! Quisquam, nisi voluptate.</p>
           <form @submit.prevent="acceder">
-            <div class="flex flex-col mb-10">
-              <input type="text" id="nomEtPrenom" v-model="nomEtPrenom" placeholder="Votre nom et prénom" @input="validerNomEtPrenom" minlength="2" maxlength="50" class="border-teal-500 border-solid border-2 p-3 w-3/4">
+            <div class="flex flex-col mb-5 lg:mb-10">
+              <input type="text" id="nomEtPrenom" v-model="nomEtPrenom" placeholder="Votre nom et prénom" @input="validerNomEtPrenom" minlength="2" maxlength="50" class="border-teal-500 border-solid border-2 p-3 md:w-5/6 lg:w-3/4">
               <span v-if="!nomEtPrenomValid" class="text-red-500">{{ nomEtPrenomErreur}}</span>
             </div>
-            <div class="flex flex-col mb-10">
-              <input type="email" id="courriel" v-model="courriel" placeholder="Votre courriel" @input="validerCourriel" minlength="2" maxlength="100" class="border-teal-500 border-solid border-2 p-3 w-3/4">
+            <div class="flex flex-col mb-5 lg:mb-10">
+              <input type="email" id="courriel" v-model="courriel" placeholder="Votre courriel" @input="validerCourriel" minlength="2" maxlength="100" class="border-teal-500 border-solid border-2 p-3 md:w-5/6 lg:w-3/4">
               <span v-if="!courrielValid" class="text-red-500">{{ courrielErreur }}</span>
             </div>
-            <button type="submit" class="bg-teal-500 border rounded p-5 text-white">Accéder maintenant</button>
+            <button type="submit" class="bg-teal-500 border rounded p-3 lg:p-5 text-white">Accéder maintenant</button>
           </form>
         </div>
       </div>
