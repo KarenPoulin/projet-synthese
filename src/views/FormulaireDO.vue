e7021f1bea520d8021f7a48e2b9c2a819cb53e36
rox perron <roxanneperron11@gmail.com>
2024-03-14 22:34:04 -0400

    Corriger les validations du formulaireDO.

--- src/views/FormulaireDO.vue
+++ src/views/FormulaireDO.vue
@@ -6,20 +6,22 @@
 
 
     <!-- Formulaire  -->
-    <form class="max-w-2xl mx-auto mt-10" @submit.prevent="soumettreFormulaireDO">
+    <form class="max-w-2xl mx-auto mt-10" @submit="submitRequest">
 
       <div class="max-w-2xl mx-auto mt-10">
-        <div>
-          <button class="bg-blue-500 text-white px-4 py-2 m-1 rounded">Annuler</button>
-          <button class="bg-blue-500 text-white px-4 py-2 m-1 rounded"><img src="../assets/icons/update-disk-30.png"
-              alt="Image de disquette">Sauvegarder</button>
+        <div class="flex justify-end">
+          <button class="bg-neutral-300  text-white px-4 py-2 m-1 rounded" @click="cancelRequest">Annuler</button>
+          <button class="bg-teal-500  text-white px-4 py-2 m-1 rounded">Sauvegarder</button>
         </div>
 
         <label for="title" class="block mb-2">Titre: </label>
-        <input id="title" v-model="title" type="text" class="border border-gray-300 p-2 w-full mb-6">
-      
+        <input id="title" v-model="formFieldsLinkedWithApi.title" @input="validateInput('title')" type="text"
+          class="border border-gray-300 p-2 w-full mb-6">
+        <span v-if="fieldsToValidate.title !== ''" class="pt-2 text-xs text-red-700">{{ fieldsToValidate.title }}</span>
 
-<!-- 
+
+
+        <!-- 
         <label for="enterprise" class="block mb-2">Entreprise: </label>
         <Select id="enterprise" v-model="enterprise" type="text" class="border border-gray-300 p-2 w-full mb-6">
           <option>1</option>
@@ -33,56 +35,73 @@
 
       <div>
         <label for="nomComplet" class="block mb-2">Nom et prénom</label>
-        <input id="nomComplet" v-model="nomComplet" class="border border-gray-300 p-2 w-full mb-6"></input>
+        <input id="nomComplet" v-model="formFieldsLinkedWithApi.nomComplet" @input="validateInput('nomComplet')"
+          class="border border-gray-300 p-2 w-full mb-6"></input>
+        <span v-if="fieldsToValidate.nomComplet !== ''" class="pt-2 text-xs text-red-700">{{ fieldsToValidate.nomComplet
+          }}</span>
 
 
 
         <label for="description" class="block mb-2">Présensention</label>
-        <textarea id="description" v-model="description" class="border border-gray-300 p-2 w-full mb-6"></textarea>
-       
+        <textarea id="description" v-model="formFieldsLinkedWithApi.description" @input="validateInput('description')"
+          class="border border-gray-300 p-2 w-full mb-6"></textarea>
+        <span v-if="fieldsToValidate.description !== ''" class="pt-2 text-xs text-red-700">{{
+      fieldsToValidate.description }}</span>
+      </div>
 
 
-        <div>
-          <label for="programme" class="block mb-2">Programme de formation</label>
-          <input id="programme" v-model="programme" type="text" class="border border-gray-300 p-2 w-full mb-6">
-        
+      <div>
+        <label for="programme" class="block mb-2">Programme de formation</label>
+        <input id="programme" v-model="formFieldsLinkedWithApi.programme" @input="validateInput('programme')"
+          type="text" class="border border-gray-300 p-2 w-full mb-6">
+        <span v-if="fieldsToValidate.programme !== ''" class="pt-2 text-xs text-red-700">{{
+      fieldsToValidate.programme }}</span>
 
 
-   <!--        <label for="requiredSkills" class="block mb-2">Exigences</label>
+
+        <!--        <label for="requiredSkills" class="block mb-2">Exigences</label>
           <textarea id="requiredSkills" v-model="requiredSkills" class="border border-gray-300 p-2 w-full mb-6"></textarea> -->
-       
 
 
-          <label for="etablissement" class="block mb-2">Établissement scolaire</label>
-          <input id="etablissement" v-model="etablissement" type="text" class="border border-gray-300 p-2 w-full mb-6">
- 
+
+        <label for="etablissement" class="block mb-2">Établissement scolaire</label>
+        <input id="etablissement" v-model="formFieldsLinkedWithApi.etablissement"
+          @input="validateInput('etablissement')" type="text" class="border border-gray-300 p-2 w-full mb-6">
+        <span v-if="fieldsToValidate.etablissement !== ''" class="pt-2 text-xs text-red-700">{{
+      fieldsToValidate.etablissement }}</span>
 
 
-          <label for="activitySector" class="block mb-2">Secteur d'activité</label>
-          <Select id="activitySector" v-model="activitySector" type="text"
-            class="border border-gray-300 p-2 w-full mb-6">
-            <option value="option1"></option>
-            <option value="option2">Technologie de l'information</option>
-            <option value="option3">Santé</option>
-            <option value="option4">Finance</option>
-            <option value="option5">Éducation</option>
-            <option value="option6">Transport et logistique</option>
-            <option value="option7">Immoblier</option>
-            <option value="option8">Tourisme</option>
-            <option value="option9">Manufacture</option>
-            <option value="option10">Énergie</option>
-            <option value="option11">Commerce au détail</option>
-          </select>
+        <label for="region" class="block mb-2">Secteur d'activité</label>
+        <select id="activitySector" v-model="formFieldsLinkedWithApi.activitySector"
+          @onchange="validateSelect(formFieldsLinkedWithApi.activitySector, 'activitySector')"
+          class="border border-gray-300 p-2 w-full mb-6">
+          <option value="">Sélectionner une option</option>
+          <option value="Technologie de l'information">Technologie de l'information</option>
+          <option value="Santé">Santé</option>
+          <option value="Finance">Finance</option>
+          <option value="Éducation">Éducation</option>
+          <option value="Transport et logistique">Transport et logistique</option>
+          <option value="Immoblier">Immoblier</option>
+          <option value="Tourisme">Tourisme</option>
+          <option value="Manufacture">Manufacture</option>
+          <option value="Énergie">Énergie</option>
+          <option value="Commerce au détail">Commerce au détail</option>
+        </select>
+        <span v-if="fieldsToValidate.activitySector !== ''" class="pt-2 text-xs text-red-700">{{
+      fieldsToValidate.activitySector }}</span>
 
-        </div>
 
         <label for="city" class="block mb-2">Ville</label>
-        <input id="city" v-model="city" class="border border-gray-300 p-2 w-full mb-6"></input>
+        <input id="city" v-model="formFieldsLinkedWithApi.city" @input="validateInput('city')"
+          class="border border-gray-300 p-2 w-full mb-6"></input>
+        <span v-if="fieldsToValidate.city !== ''" class="pt-2 text-xs text-red-700">{{ fieldsToValidate.city }}</span>
+
 
 
 
         <label for="region" class="block mb-2">Région</label>
-        <Select id="region" v-model="region" type="text" class="border border-gray-300 p-2 w-full mb-6">
+        <Select id="region"  v-model="formFieldsLinkedWithApi.region"
+          @onchange="validateSelect(formFieldsLinkedWithApi.region, 'region')" type="text" class="border border-gray-300 p-2 w-full mb-6">
           <option value="option1"></option>
           <option value="option2">Québec</option>
           <option value="option3">Trois-Rivière</option>
@@ -90,36 +109,46 @@
           <option value="option5">Sherbrooke</option>
           <option value="option6">Gatineau</option>
         </select>
-       
+        <span v-if="fieldsToValidate.region !== ''" class="pt-2 text-xs text-red-700">{{
+      fieldsToValidate.region }}</span>
       </div>
-
+      
       <label for="skills" class="block mb-2">Compétences</label>
-      <textarea id="skills" v-model="skills" class="border border-gray-300 p-2 w-full mb-6"></textarea>
-     
+      <textarea id="skills " v-model="formFieldsLinkedWithApi.skills" @input="validateInput('skills')"
+        class="border border-gray-300 p-2 w-full mb-6"></textarea>
+      <span v-if="fieldsToValidate.skills !== ''" class="pt-2 text-xs text-red-700">{{ fieldsToValidate.skills
+        }}</span>
+
 
 
       <h2 class="p-2 w-full mb-6">Information sur le stage</h2>
       <label for="intershipType" class="block mb-2">Type de stage</label>
-      <select id="intershipType" v-model="intershipType" class="border border-gray-300 p-2 w-full mb-6">
+      <select id="intershipType"  v-model="formFieldsLinkedWithApi.internshipType"
+          @onchange="validateSelect(formFieldsLinkedWithApi.internshipType, 'nternshipType')" type="text" class="border border-gray-300 p-2 w-full mb-6">class="border border-gray-300 p-2 w-full mb-6">
         <option value="option1"></option>
         <option value="option2">Temps plein</option>
         <option value="option3">Temps partiel</option>
       </select>
+      <span v-if="fieldsToValidate.internshipType !== ''" class="pt-2 text-xs text-red-700">{{
+      fieldsToValidate.internshipType }}</span>
   
+
 
 
       <label for="startDate" class="block mb-2">Date de début</label>
       <input id="startDate" v-model="startDate" type="date" class="border border-gray-300 p-2 w-full mb-6">
-      
 
 
-      <label for="weeklyWorkHours" class="block mb-2">Nombre d'heures par semaine</label>
-      <input id="weeklyWorkHours" v-model="weeklyWorkHours" type="number" class="border border-gray-300 p-2 w-full mb-6">
+
+      <!--       <label for="weeklyWorkHours" class="block mb-2">Nombre d'heures par semaine</label>
+      <input id="weeklyWorkHours" v-model="weeklyWorkHours" type="number"
+        class="border border-gray-300 p-2 w-full mb-6"> -->
+
 
 
       <label for="endDate" class="block mb-2">Date de fin</label>
       <input id="endDate" v-model="endDate" type="date" class="border border-gray-300 p-2 w-full mb-6">
- 
+
 
 
       <h2 class="p-2 w-full mb-2">Rémumération</h2>
@@ -132,20 +161,20 @@
 
 
 
-      <label for="additionalInformatione" class="p-2 w-full mb-2">Information supplémentaire</label>
-      <textarea id="additionalInformation" v-model="additionalInformation"
-        class="border border-gray-300 p-2 w-full mb-6"></textarea>
- 
+      <label for="additionalInformation" class="p-2 w-full mb-2">Information supplémentaire</label>
+      <textarea class="border border-gray-300 p-2 w-full mb-6"></textarea>
+
+
 
 
       <input
-        class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+        class="block w-full text-sm text-neutral-950 border border-neutrla-300 rounded-lg cursor-pointer bg-neutral-100 bg-neutral-100 bg-neutral-300"
         aria-describedby="file_input_help" id="file_input" type="file">
 
-      <div>
-        <button class="bg-blue-500 text-white px-4 py-2 m-1 rounded">Annuler</button>
-        <button class="bg-blue-500 text-white px-4 py-2 m-1 rounded"><img src="../assets/icons/update-disk-30.png"
-            alt="Image de disquette">Sauvegarder</button>
+
+      <div class="flex justify-end">
+        <button class="bg-neutral-300 text-white px-4 py-2 m-1 rounded">Annuler</button>
+        <button class="bg-teal-500 text-white px-4 py-2 m-1 rounded" @click="cancelRequest">Sauvegarder</button>
       </div>
 
     </form>
@@ -156,19 +185,19 @@
 import { reactive, ref } from 'vue';
 // Création des variables requises pour la validation du formulaire
 
-const formFields = reactive({
+const formFieldsLinkedWithApi = reactive({
   title: '',
   nomComplet: '',
   description: '',
   programme: '',
   etablissement: '',
-  activitySector: '', 
+  activitySector: '',
   city: '',
   region: '',
   skills: '',
   internshipType: '',
   startDate: '',
-  weeklyWorkHourse: '',
+  weeklyWorkHours: '',
   endDate: '',
   aDiscuter: '',
   remunere: '',
@@ -179,84 +208,108 @@ const formFields = reactive({
 const errorMessage = reactive({
   empty: 'Le champ ne peut pas être vide',
   minCharacters: 'Le champ doit avoir au moins 5 caractères',
-  maxCharacters: 'Le champ ne peut pas dépasser 100 caractères',
+  maxCharacters: 'Le champ ne peut pas dépasser 300 caractères',
+  option: 'Le champ doit avoir une option valide'
 });
 
 const fieldsToValidate = reactive({
-  titre: '',
-  entreprise: '',
+  title: '',
   nomComplet: '',
-  presentation: '',
+  description: '',
   programme: '',
-  exigences: '',
   etablissement: '',
-  secteurActivite: '',
-  ville: '',
+  activitySector: '',
+  city: '',
   region: '',
-  competences: '',
-  typeStage: '',
-  dateDebut: '',
-  heuresSemaine: '',
-  dateFin: '',
+  skills: '',
+  internshipType: '',
+  startDate: '',
+  weeklyWorkHours: '',
+  endDate: '',
   aDiscuter: '',
   remunere: '',
   nonRemunere: '',
-  infoSupplementaire: ''
+  additionalInformation: ''
 });
 
-let validRequestForm = ref(false)
+let isFormValid = ref(false);
 
-// Fonction pour valider les champs de type input 
-
-function validateInput(input, inputName) {
-  if (input === '') {
-    formFields[inputName] = errorMessage.vide;
-    return;
+// Fonction pour valider les champs de type input
+function validateInput(input, field) {
+  if (input.trim() === "") {
+    return errorMessage.empty;
   }
   if (input.length < 5) {
-    formFields[inputName] = errorMessage.minCharacters;
-    return;
+    return errorMessage.minCharacters;
   }
-  if (input.length > 100) {
-    formFields[inputName] = errorMessage.maxCharacters;
-    return;
+  if (input.length > 300) {
+    return errorMessage.maxCharacters;
   }
-  formFields[inputName] = '';
+
+  // Réinitialiser le message d'erreur du champ si la validation réussit
+  fieldsToValidate[field] = '';
+
+  return '';
 }
+
+// Fonction pour valider le champ de type select
+function validateSelect(select, field) {
+  if (select.trim() === "") {
+    return errorMessage.option;
+  }
+
+  fieldsToValidate[field] = '';
+
+  return '';
+}
+
+// Fonction pour valider les champs de type date
+// Fonction pour valider les champs de type nombre
+// Fonction pour valider les champs de type checkbox
+
 
 // Fonction pour soumettre le formulaire 
 
-const submitResquest = () => {
+const submitRequest = () => {
   event.preventDefault();
-  validateInput(formFields.title, 'title');
-  validateInput(formFields.nomComplet, 'nomComplet');
-  validateInput(formFields.presentation, 'presentation');
-  validateInput(formFields.programme, 'programme');
-  validateInput(formFields.etablissement, 'etablissement');
-  validateInput(formFields.city, 'city');
-  validateInput(formFields.skills, 'skills');
-  validateInput(formFields.additionalInformation, 'additionalInformation');
+  fieldsToValidate.title = validateInput(formFieldsLinkedWithApi.title, 'title');
+  fieldsToValidate.nomComplet = validateInput(formFieldsLinkedWithApi.nomComplet, 'nomComplet');
+  fieldsToValidate.description = validateInput(formFieldsLinkedWithApi.description, 'description');
+  fieldsToValidate.programme = validateInput(formFieldsLinkedWithApi.programme, 'programme');
+  fieldsToValidate.etablissement = validateInput(formFieldsLinkedWithApi.etablissement, 'etablissement');
+  fieldsToValidate.city = validateInput(formFieldsLinkedWithApi.city, 'city');
+  fieldsToValidate.skills = validateInput(formFieldsLinkedWithApi.skills, 'skills');
+  fieldsToValidate.activitySector = validateSelect(formFieldsLinkedWithApi.activitySector, 'activitySector');
+  fieldsToValidate.region = validateSelect(formFieldsLinkedWithApi.region, 'region');
+   fieldsToValidate.internshipType = validateSelect(formFieldsLinkedWithApi.internshipType, 'internshipType');
 
-  if (
-    fieldsToValidate.title === '' && 
-    fieldsToValidate.nomComplet === '' && 
-    fieldsToValidate.description=== '' && 
-    fieldsToValidate.programme === '' && 
-    fieldsToValidate.etablissement === '' &&
-    fieldsToValidate.city=== '' && 
-    fieldsToValidate.skills === '' && 
-    fieldsToValidate.additionalInformation === ''  
-     ) {
-      fvalidRequestForm = true;
-     } else {
-      validRequestForm = false;
-     }
+  isFormValid.value = Object.values(fieldsToValidate).every(value => value === '');
 };
 
-// Fonction pour annuler le formulaire 
+// Fonction  sur le bouton annuler du formulaire 
 const cancelRequest = () => {
-      
-    };
+  // Réinitialiser les champs du formulaire
+  formFieldsLinkedWithApi.title = '';
+  formFieldsLinkedWithApi.nomComplet = '';
+  formFieldsLinkedWithApi.description = '';
+  formFieldsLinkedWithApi.programme = '';
+  formFieldsLinkedWithApi.etablissement = '';
+  formFieldsLinkedWithApi.city = '';
+  formFieldsLinkedWithApi.skills = '';
+  formFieldsLinkedWithApi.additionalInformation = '';
+
+  // Réinitialiser les messages d'erreur
+  for (let field in fieldsToValidate) {
+    fieldsToValidate[field] = '';
+  }
+};
+
+
+
+
+
+
+
 
 </script>
 
