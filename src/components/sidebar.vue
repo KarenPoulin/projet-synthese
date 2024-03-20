873eb292a410d24a31f57cefa4a7c07ff8481373
Natissa-DidyToure <natissadidy@gmail.com>
2024-03-20 16:00:08 -0400

    quelques modifications au menu d'actions

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,10 +1,10 @@
 <template>
-    <aside class=" bg-white min-h-full flex flex-col items-center pb-2 space-y-7">
+<aside class=" bg-white min-h-full flex flex-col items-center pb-2 space-y-7">
 
     <!-- Logo -->
 
         <div class="w-full flex justify-start items-start mt-0 mb-12">
-      <img src="../assets/img/logo.png" alt="Logo" class="h-30 w-full ">
+      <img src="../assets/img/logo.png" alt="Logo" class="h-30 w-full">
     </div>
 
 
@@ -16,12 +16,14 @@
   
 <!--Bouton Tableau de Bord-->
 <a href="/app/tableaudebord" class="w-full flex items-center gap-x-1.5 group select-none">
-    <div class="w-1 rounded-xl h-12 bg-transparent transition-colors duration-200 relative overflow-hidden">
-        <div class="absolute top-0 right-0 bg-teal-500 transition-all duration-300 group-hover:translate-y-0 translate-y-0" style="width: 0.75rem; height: 102%;"></div>
-    </div>
+
+<div class="w-1 rounded-xl h-12 bg-transparent transition-colors duration-200 relative overflow-hidden">
+<div class="absolute top-0 right-0 bg-teal-500 transition-all duration-300 group-hover:translate-y-0 translate-y-0 h-full w-3"></div>
+</div>
+
     
-    <div class="bg-white/10 text-white group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm">
-        <img src="../assets/icons/sidebar-danshboard-24.png" alt="Dashboard" class="h-5 w-5 fill-teal-500 dark:fill-gray-600 transition-colors duration-200" />
+<div class="bg-white/10 text-white group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm">
+<img src="../assets/icons/sidebar-danshboard-24.png" alt="Dashboard" class="h-5 w-5 fill-teal-500 dark:fill-gray-600 transition-colors duration-200" />
         <span class="font-QuicksandMedium font-bold text-teal-500">Tableau de bord</span>
     </div>
 </a>
