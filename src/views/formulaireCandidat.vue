0f1f11c49be982de781cd2b9ac92281d288505bf
Jonathan Claes <115749108+JoMesss123@users.noreply.github.com>
2024-03-20 18:02:38 -0400

    modif bouton

--- src/views/formulaireCandidat.vue
+++ src/views/formulaireCandidat.vue
@@ -20,6 +20,7 @@
             class="btn-secondary mr-2 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
             @click="cancelForm">Annuler</button>
         </RouterLink>
+        <router-link to="/app/candidats">
         <button type="submit"
           class="btn-primary focus:outline-none text-white bg-fuchsia-800  hover:bg-fuchsia-900 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 inline-flex"
           :disabled="!isFormValid">
@@ -30,6 +31,7 @@
               clip-rule="evenodd" />
           </svg>
           {{ editing ? 'Mettre à jour' : 'Sauvegarder' }}</button>
+        </router-link>
       </div>
 
       <div class="nom_poste mb-4">
@@ -107,6 +109,7 @@
             class="btn-secondary mr-2 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
             @click="cancelForm">Annuler</button>
         </router-link>
+        <router-link to="/app/candidats">
         <button type="submit"
           class="btn-primary focus:outline-none text-white bg-fuchsia-800  hover:bg-fuchsia-900 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 inline-flex"
           :disabled="!isFormValid">
@@ -117,6 +120,7 @@
               clip-rule="evenodd" />
           </svg>
           {{ editing ? 'Mettre à jour' : 'Sauvegarder' }}</button>
+        </router-link>
       </div>
     </form>
   </div>
