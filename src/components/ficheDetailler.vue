0e2cd88044d83d7fec4350231306cc555df74232
Mariama Diaby <diabymariama414@gmail.com>
2024-03-19 22:19:03 -0700

    reutilisation de component terminer

--- src/components/ficheDetailler.vue
+++ src/components/ficheDetailler.vue
@@ -1,81 +1,312 @@
 <template>
-  <main class="ficheDetaillee" :class="{ 'modal-open': etatDuModal }">
- <!-- Cette div restera sur cette page de fiche detailler -->
+  <main class="ficheDetaillee">
     <div class="ficheDetaillee__conteneur-titres-principaux">
-      <div class="ficheDetaillee__bloc-style"></div>  // Mettre une class de style conditionnelle
-      <div class="ficheDetaillee__titres">
-        <span class="ficheDetaillee__sous-titre">Demande de stage</span>  // Mettre props
-        <h1 class="ficheDetaillee__titre">{{ title }}</h1>  // Mettre un props
+      <div class="ficheDetaillee__bloc-style" 
+        :class="{
+          'colorBgBlue': isPageDetaillerDemandeDeStage, 
+          'colorBgRed': isPageDetaillerDemandeOffre, 
+          'colorBgPurple': isPageDetaillerCandidat
+        }">
+        </div>
+
+      <div class="ficheDetaillee__titre">
+        <span class="ficheDetaillee__sous-titre">{{ sousTitre }}</span>
+        <h1 class="ficheDetaillee__titre">{{ mainTitre }}</h1>
       </div>
     </div>
-  </main>
+
+    <div class="ficheDetaillee__card">
+      <div class="ficheDetaillee__conteneur-informations">
+
+      <div class="ficheDetaillee__conteneur-titre" >
+        <h2 class="ficheDetaillee__nom-personne"  
+          :class="{
+            'colorBlue': isPageDetaillerDemandeDeStage, 
+            'colorRed': isPageDetaillerDemandeOffre, 
+            'colorPurple': isPageDetaillerCandidat
+          }">
+          {{ ficheTitreOne }} {{ ficheTitreTwo }}
+        </h2>
+        <p>{{ ficheDescription }}</p> 
+      </div>
+
+      <div class="ficheDetaillee__conteneur-icônes-mode">
+      <i class="ficheDetaillee__icône-consulter fa fa-check"></i>
+      <i class="ficheDetaillee__icône-editer fas fa-edit" @click="emitRedirectionPageEdition"></i>
+      <i class="ficheDetaillee__icône-supprimer fas fa-trash" @click="emitToggleModal"></i>
+      <slot></slot>
+      </div>
+
+        <div class="ficheDetaillee__conteneur-informations-générales" v-if="isPageDetaillerDemandeDeStage">
+          
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Programme de formation</h3>
+              <span class="ficheDetaillee__contenu-info">{{ programmeDeFormation }}</span>
+            </div>
+          </div>
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Secteur d'activité</h3>
+              <span class="ficheDetaillee__contenu-info">{{ secteurDeActiviter }}</span>
+            </div>
+          </div>
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Compétences</h3>
+              <span class="ficheDetaillee__contenu-info">{{ competence }}</span>
+            </div>
+          </div>
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Établissement d'enseignement</h3>
+              <span class="ficheDetaillee__contenu-info">{{ etablissementEnseignement }}</span>
+            </div>
+          </div>
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Ville</h3>
+              <span class="ficheDetaillee__contenu-info">{{ ville }}</span>
+            </div>
+          </div>
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Région</h3>
+              <span class="ficheDetaillee__contenu-info">{{ region }}</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="ficheDetaillee__conteneur-informations-générales" v-if="!isPageDetaillerDemandeDeStage && !isPageDetaillerCandidat">
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Formation demandées</h3>
+              <span class="ficheDetaillee__contenu-info">{{ diplomeDemandees }}</span>
+            </div>
+          </div>
+
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Exigences</h3>
+              <span class="ficheDetaillee__contenu-info">{{ exigencesText }}</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="ficheDetaillee__conteneur-informations-générales" v-if="isPageDetaillerCandidat">
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Adresse</h3>
+              <span class="ficheDetaillee__contenu-info">{{ adresse }}</span>
+            </div>
+          </div>
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Ville</h3>
+              <span class="ficheDetaillee__contenu-info">{{ ville }}</span>
+            </div>
+          </div>
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Province</h3>
+              <span class="ficheDetaillee__contenu-info">{{ province }}</span>
+            </div>
+          </div>
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Code postal</h3>
+              <span class="ficheDetaillee__contenu-info">{{ codePostal }}</span>
+            </div>
+          </div>
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Telephone</h3>
+              <span class="ficheDetaillee__contenu-info">{{ telephone }}</span>
+            </div>
+          </div>
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Courriel</h3>
+              <span class="ficheDetaillee__contenu-info">{{ courriel }}</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="ficheDetaillee__conteneur-titre" v-if="isPageDetaillerDemandeDeStage">
+          <h2 class="ficheDetaillee__titre-info-stage" :class="{'colorBlue': isPageDetaillerDemandeDeStage }">
+           Informations sur le stage 
+          </h2>
+        </div>
+
+        <div class="ficheDetaillee__conteneur-titre" v-if="isPageDetaillerDemandeOffre">
+          <h2 class="ficheDetaillee__titre-info-stage" :class="{'colorRed': isPageDetaillerDemandeOffre}">
+           Informations sur le stage recherché
+          </h2>
+        </div>
+
+      <div class="ficheDetaillee__conteneur-titre" v-if="isPageDetaillerCandidat">
+        <h2 class="ficheDetaillee__titre-info-stage" :class="{'colorPurple': isPageDetaillerCandidat}">
+          Informations personnelles
+        </h2>
+      </div>
+
+      <div class="ficheDetaillee__conteneur-informations-specifiques" v-if="isPageDetaillerDemandeDeStage || isPageDetaillerDemandeOffre"> 
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Type de stage</h3>
+              <span class="ficheDetaillee__contenu-info">{{ typeDeStage }}</span>
+            </div>
+          </div>
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Nombre d'heures par semaine</h3>
+              <span class="ficheDetaillee__contenu-info">{{ nombreHeures }}</span>
+            </div>
+          </div>
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Rémunération</h3>
+              <span class="ficheDetaillee__contenu-info">{{ remuneration }}</span>
+            </div>
+          </div>
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Date de début</h3>
+              <span class="ficheDetaillee__contenu-info">{{ dateDeDebut }}</span>
+            </div>
+          </div>
+          <div class="ficheDetaillee__conteneur-information">
+            <div class="ficheDetaillee__bloc-style"></div>
+            <div class="ficheDetaillee__infos">
+              <h3 class="ficheDetaillee__titre-info">Date de fin</h3>
+              <span class="ficheDetaillee__contenu-info">{{ dateDeFin }}</span>
+            </div> 
+          </div>
+        </div>
+
+
+      <div class="ficheDetaillee__conteneur-titre" v-if="isPageDetaillerDemandeDeStage || isPageDetaillerDemandeOffre">
+        <h2 class="ficheDetaillee__titre-info-supplementaire" 
+          :class="{
+            'colorBlue': isPageDetaillerDemandeDeStage,
+            'colorRed': isPageDetaillerDemandeOffre}">
+          Informations supplémentaires
+        </h2>
+        <p> {{ informationsSupplémentairesText }} </p> 
+      </div>
+
+      <div class="ficheDetaillee__conteneur-informations-supplementaires" v-if="isPageDetaillerDemandeDeStage">
+        <button type="button" class="ficheDetaillee__bouton-cta">
+          <i class="ficheDetaillee__icône fas fa-cloud-download-alt"></i>
+          <span>
+            Télécharger le C.V.
+          </span>
+        </button>
+      </div>
+      </div>
+    </div>
+</main>
 </template>
 
 
 <script setup>
-import { ref, onMounted } from 'vue'
-import { useRouter } from 'vue-router'
-import modalSuppression from './modalSuppression.vue';
-// Importer et configurer les donnees get par id de l'api.
-// Importer et configurer les donnees de delete de l'api.
+import { defineProps } from 'vue';
 
-const identifiant = ref(null)
-const route = useRouter()
-const etatDuModal = ref(false)
+const props = defineProps({
+  sousTitre: String,
+  mainTitre: String,
+  ficheTitreOne: String,
+  ficheTitreTwo: String,
+  ficheDescription: String,
+  programmeDeFormation: String,
+  secteurDeActiviter: String,
+  competence: String,
+  etablissementEnseignement: String,
+  ville: String,
+  region: String,
+  diplomeDemandees: String,
+  exigencesText: String,
+  adresse: String,
+  province: String,
+  codePostal: String,
+  telephone: String,
+  courriel: String,
+  typeDeStage: String,
+  nombreHeures: String,
+  remuneration: String,
+  dateDeDebut: String,
+  dateDeFin: String,
+  informationsSupplémentairesText: String,
+  isPageDetaillerDemandeDeStage: Boolean,
+  isPageDetaillerDemandeOffre: Boolean,
+  isPageDetaillerCandidat: Boolean,
+});
+// Définition des émetteurs pour les icônes
+const emits = defineEmits([
+  'redirectionPageEdition',
+  'toggleModal',
+  'annulerSuppressionIcone',
+  'annulerSuppression'
+]);
 
-// Appel de la fonction get par id pour afficher les detailles
-// onMounted(() => {
-//   identifiant.value = route.params.id;
-// })
-
-// Fonction de redirection a la page edition
-// const redirectionPageEdition = () => {
-//   const editIdentifiant = `/edit-page?id=${identifiant.value}`;
-//   route.push(editIdentifiant)
-// }
-
-const annulerSuppressionIcone = () => {
-  etatDuModal.value = false;
-  console.log(etatDuModal.value)
+// Émetteur pour rediriger icone edition vers une autre page
+const emitRedirectionPageEdition = () => {
+  emits('redirectionPageEdition');
 }
 
-const annulerSuppression = () => {
-  etatDuModal.value = false;
-  console.log(etatDuModal.value)
-}
-
-// Appel de la fonction supprimer de lapi avec le id a linterieur pour supprimer 
-// const suppressionConfirmee = () => {
-//   etatDuModal.value = false;
-//   console.log(etatDuModal.value)
-// }
-
-const toggleModal = () => {
-  etatDuModal.value = !etatDuModal.value;
+// Émetteur pour activer/désactiver le modal
+const emitToggleModal = () => {
+  emits('toggleModal');
 }
 </script>
-    
-<style scoped>
 
-.modal-container {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: flex-start; 
+
+<style scoped>
+.colorBlue{
+  color: rgb(20 184 166);;
+}
+.colorRed{
+  color: red;
+}
+.colorPurple{
+  color: palevioletred;
+}
+.colorBgBlue{
+  background-color: rgb(20 184 166);;
+}
+.colorBgRed{
+  background-color: red;
+}
+.colorBgPurple{
+  background-color: palevioletred;
 }
 
 .ficheDetaillee {
-  position: relative; 
-  z-index: 1;
+  border: 2px solid red;
   max-width: 1100px;
   margin: auto;
 }
 
 .ficheDetaillee__conteneur-titres-principaux {
+  border: 2px solid blue;
   margin-bottom: 100px;
   display: flex;
 }
@@ -84,7 +315,6 @@ const toggleModal = () => {
 .ficheDetaillee__bloc-style {
   width: 10px;
   height: 75px;
-  background-color:rgb(133 77 14);
   margin-right: 10px;
 }
 
@@ -155,7 +385,6 @@ const toggleModal = () => {
 
 .ficheDetaillee__conteneur-titre
 .ficheDetaillee__nom-personne {
-  color: rgb(20 184 166);
   font-size: 40px;
   font-weight: 600;
   margin-bottom: 40px;
@@ -163,7 +392,6 @@ const toggleModal = () => {
 
 .ficheDetaillee__conteneur-titre
 .ficheDetaillee__titre-info-stage{
-  color: rgb(20 184 166);
   font-size: 20px;
   font-weight: 600;
 }
@@ -174,7 +402,6 @@ const toggleModal = () => {
 
 .ficheDetaillee__conteneur-titre
 .ficheDetaillee__titre-info-supplementaire{
-  color: rgb(20 184 166);
   font-size: 20px;
   font-weight: 600;
   margin-bottom: 40px;
--- src/views/DemandesDeStages.vue
+++ src/views/DemandesDeStages.vue
@@ -1,10 +1,70 @@
 <template>
       <ficheDetailler
-      
+      sousTitre="allo"
+      mainTitre="allo"
+      ficheTitreOne="allo"
+      ficheTitreTwo="allo"
+      ficheDescription="allo"
+      @emitRedirectionPageEdition=""
+      @emitToggleModal="toggleModal"
+      programmeDeFormation="allo"
+      secteurDeActiviter="allo"
+      competence="allo"
+      etablissementEnseignement="allo"
+      ville="allo"
+      region="allo"
+      diplomeDemandees="allo"
+      exigencesText="allo"
+      adresse="allo"
+      province="allo"
+      codePostal="allo"
+      telephone="allo"
+      courriel=""
+      typeDeStage="allo"
+      nombreHeures="allo"
+      remuneration="allo"
+      dateDeDebut="allo"
+      dateDeFin="allo"
+      informationsSupplémentairesText="allo"
+      :isPageDetaillerDemandeDeStage="true"
+      :isPageDetaillerDemandeOffre="false"
+      :isPageDetaillerCandidat="false">
+      <modalSuppression
+          :etatDuModal="true" 
+          @suppressionAnnuleeIcone="annulerSuppressionIcone"
+          @suppressionAnnulee="annulerSuppression"
+          @confirmationSuppression="suppressionConfirmee"
+          title="title"
+          firstName="firstName"
+          lastName="lastName"
       />
-  </template>
-  
-  <script setup>
-    import ficheDetailler from '@/components/ficheDetailler.vue';
-  </script>
-  
+      </ficheDetailler>
+</template>
+
+<script setup>
+import ficheDetailler from '@/components/ficheDetailler.vue';
+import modalSuppression from '@/components/modalSuppression.vue';
+import { ref } from 'vue'
+
+const etatDuModal = ref(false)
+
+const annulerSuppressionIcone = () => {
+  etatDuModal.value = false;
+  console.log(etatDuModal.value)
+}
+
+const annulerSuppression = () => {
+  etatDuModal.value = false;
+  console.log(etatDuModal.value)
+}
+
+const suppressionConfirmee = () => {
+  etatDuModal.value = false;
+  // Information de lapi pour la suppression
+  console.log(etatDuModal.value)
+}
+
+const toggleModal = () => {
+  etatDuModal.value = !etatDuModal.value;
+}
+</script>
