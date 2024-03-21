afc0d3a62ad3ffda0664c173150d775569349bb5
Natissa-DidyToure <natissadidy@gmail.com>
2024-03-21 14:47:19 -0400

    Amélioré la sidebar : ajout d'un nouveau logo pour une meilleure résolution, routage configuré pour chaque bouton, implémenté active-class pour le style actif, ajouté un effet de survol sur la gauche, et supprimé les styles inutiles

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,96 +1,94 @@
 <template>
-<aside class=" bg-white min-h-full flex flex-col items-center pb-2 space-y-7">
+    <div class="min-h-screen flex flex-col lg:w-[310px] bg-white w-full ">
+        <!-- Logo en haut de la sidebar -->
+        <div class="w-full flex justify-center items-center my-6 mt-0 mb-10">
+            <img src="../assets/img/logo2.png" alt="Logo" class="w-full object-contain">
+        </div>
 
-    <!-- Logo -->
+        <!-- Liste des boutons de la sidebar -->
+        <ul class="flex-grow pt-10">
+            <!-- Bouton 1 -->
+            <li class="pt-2">
+                <router-link :to="{ name: 'tableaudebord' }"
+                    class="flex items-center justify-between p-4 transition-all duration-300 text-teal-500 router-link"
+                    active-class="active">
+                    <img src="../assets/icons/sidebar-danshboard-24.png" alt="Icon 1" class="h-6 w-6">
+                    <!-- Icône du bouton -->
+                    <span class="link-text">Tableau de bord</span>
+                    <span class="indicator w-0 h-full bg-transparent transition-all duration-300"></span>
+                    <!-- Effet de survol -->
+                </router-link>
+            </li>
+            <!--Bouton 2-->
+            <li class="pt-2">
+                <router-link :to="{ name: 'demandesdestages' }"
+                    class="flex items-center justify-between p-4 transition-all duration-300 text-yellow-600 router-link"
+                    active-class="active">
+                    <img src="../assets/icons/sidebar-request-24.png" alt="Icon 2" class="h-6 w-6">
+                    <!-- Icône du bouton -->
+                    <span class="link-text">Demandes de stages</span>
+                    <span class="indicator w-0 h-full bg-transparent transition-all duration-300"></span>
+                    <!-- Effet de survol -->
+                </router-link>
+            </li>
+            <!--Bouton 3-->
+            <li class="pt-2">
+                <router-link :to="{ name: 'offresdestages' }"
+                    class="flex items-center justify-between p-4 transition-all duration-300 text-red-800 router-link"
+                    active-class="active">
+                    <img src="../assets/icons/sidebar-offer.png" alt="Icon 3" class="h-6 w-6"> <!-- Icône du bouton -->
+                    <span class="link-text">Offres de stages</span>
+                    <span class="indicator w-0 h-full bg-transparent transition-all duration-300"></span>
+                    <!-- Effet de survol -->
+                </router-link>
+            </li>
+            <!--Bouton 4-->
+            <li class="pt-2">
+                <router-link :to="{ name: 'candidats' }"
+                    class="flex items-center justify-between p-4 transition-all duration-300 text-fuchsia-800 router-link"
+                    active-class="active">
+                    <img src="../assets/icons/sidebar-candidate-24.png" alt="Icon 4" class="h-6 w-6">
+                    <!-- Icône du bouton -->
+                    <span class="link-text">Candidats</span>
+                    <span class="indicator w-0 h-full bg-transparent transition-all duration-300"></span>
+                    <!-- Effet de survol -->
+                </router-link>
+            </li>
+            <!--Bouton 5-->
+            <li class="pt-2">
+                <router-link :to="{ name: 'entreprises' }"
+                    class="flex items-center justify-between p-4 transition-all duration-300 text-blue-400 router-link"
+                    active-class="active">
+                    <img src="../assets/icons/sidebar-enterprise-24.png" alt="Icon 5" class="h-6 w-6">
+                    <!-- Icône du bouton -->
+                    <span class="link-text">Entreprises</span>
+                    <span class="indicator w-0 h-full bg-transparent transition-all duration-300"></span>
+                    <!-- Effet de survol -->
+                </router-link>
+            </li>
 
-        <div class="w-full flex justify-start items-start mt-0 mb-12">
-      <img src="../assets/img/logo.png" alt="Logo" class="h-30 w-full">
+
+        </ul>
     </div>
-
-
-    <!-- Side bar -->
-
-
-    <div class="w-full pr-3 flex flex-col gap-y-1 text-gray-500 fill-gray-500 text-sm">
-        
-  
-<!--Bouton Tableau de Bord-->
-<a href="/app/tableaudebord" class="w-full flex items-center gap-x-1.5 group select-none">
-
-<div class="w-1 rounded-xl h-12 bg-transparent transition-colors duration-200 relative overflow-hidden">
-<div class="absolute top-0 right-0 bg-teal-500 transition-all duration-300 group-hover:translate-y-0 translate-y-0 h-full w-3"></div>
-</div>
-
-    
-<div class="bg-white/10 text-white group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm">
-<img src="../assets/icons/sidebar-danshboard-24.png" alt="Dashboard" class="h-5 w-5 fill-teal-500 dark:fill-gray-600 transition-colors duration-200" />
-        <span class="font-QuicksandMedium font-bold text-teal-500">Tableau de bord</span>
-    </div>
-</a>
-
-<!--Bouton Demandes de stages-->
-<a href="/app/demandesdestages" class="w-full flex items-center gap-x-1.5 group select-none">
-            <div class="w-1 rounded-xl h-12 bg-transparent transition-colors duration-200 relative overflow-hidden">
-                <div class="absolute top-0 left-0 w-full h-[102%] translate-y-full group-hover:translate-y-0 bg-yellow-600 transition-all duration-300"></div>
-            </div>
-            <div class="group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm">
-                <img src="../assets/icons/sidebar-request-24.png" class="h-5 w-5 group-hover:fill-red-600 dark:fill-gray-600 transition-colors duration-200">
-
-                </img>
-                <span class="font-QuicksandMedium text-yellow-600">Demandes de stages</span>
-            </div>
-</a>
-
-<!--Bouton Offres-->
-<a href="/app/offresdestages" class="w-full flex items-center gap-x-1.5 group select-none">
-            <div class="w-1 rounded-xl h-12 bg-transparent transition-colors duration-200 relative overflow-hidden">
-                <div class="absolute top-0 left-0 w-full h-[102%] translate-y-full group-hover:translate-y-0 bg-red-800 transition-all duration-300"></div>
-            </div>
-            <div class="group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm">
-                <img src="../assets/icons/sidebar-offer.png" class="h-5 w-5 group-hover:fill-red-600 dark:fill-gray-600 transition-colors duration-200">
-
-                </img>
-                <span class="font-QuicksandMedium text-red-800">Offres de stages</span>
-            </div>
-</a>
-<!--Bouton Candidats-->
-<a href="/app/candidats" class="w-full flex items-center gap-x-1.5 group select-none">
-            <div class="w-1 rounded-xl h-12 bg-transparent transition-colors duration-200 relative overflow-hidden">
-                <div class="absolute top-0 left-0 w-full h-[102%] translate-y-full group-hover:translate-y-0 bg-fuchsia-800 transition-all duration-300"></div>
-            </div>
-            <div class="group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm">
-                <img src="../assets/icons/sidebar-candidate-24.png" class="h-5 w-5 group-hover:fill-red-600 dark:fill-gray-600 transition-colors duration-200">
-
-                </img>
-                <span class="font-QuicksandMedium text-fuchsia-800">Candidats</span>
-            </div>
-</a>
-<!--Entreprises-->
-<a href="/app/entreprises" class="w-full flex items-center gap-x-1.5 group select-none">
-            <div class="w-1 rounded-xl h-12 bg-transparent transition-colors duration-200 relative overflow-hidden">
-                <div class="absolute top-0 left-0 w-full h-[102%] translate-y-full group-hover:translate-y-0 bg-blue-400 transition-all duration-300"></div>
-            </div>
-            <div class="group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm">
-                <img src="../assets/icons/sidebar-enterprise-24.png" class="h-5 w-5 group-hover:fill-red-600 dark:fill-gray-600 transition-colors duration-200">
-
-                </img>
-                <span class="font-QuicksandMedium text-blue-400">Entreprises</span>
-            </div>
-</a>
-
-
-
-
-
-
-
-    </div>
-
-
-</aside>
-
 </template>
 
 <script setup>
 
 </script>
+
+<style scoped>
+.router-link:hover .link-text,
+.router-link.active .link-text {
+    font-weight: bold;
+}
+
+.router-link:hover .indicator,
+.router-link.active .indicator {
+    width: 2px;
+    background-color: currentColor;
+}
+
+.active {
+    border-right: 4px solid;
+}
+</style>
