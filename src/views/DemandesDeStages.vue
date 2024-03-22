42ef8a724145be238b44ba841775ac78b61b9223
Mariama Diaby <diabymariama414@gmail.com>
2024-03-21 19:01:51 -0700

    changement pageDemandeDeStage

--- src/views/DemandesDeStages.vue
+++ src/views/DemandesDeStages.vue
@@ -1,70 +1,17 @@
 <template>
-      <ficheDetailler
-      sousTitre="allo"
-      mainTitre="allo"
-      ficheTitreOne="allo"
-      ficheTitreTwo="allo"
-      ficheDescription="allo"
-      @emitRedirectionPageEdition=""
-      @emitToggleModal="toggleModal"
-      programmeDeFormation="allo"
-      secteurDeActiviter="allo"
-      competence="allo"
-      etablissementEnseignement="allo"
-      ville="allo"
-      region="allo"
-      diplomeDemandees="allo"
-      exigencesText="allo"
-      adresse="allo"
-      province="allo"
-      codePostal="allo"
-      telephone="allo"
-      courriel=""
-      typeDeStage="allo"
-      nombreHeures="allo"
-      remuneration="allo"
-      dateDeDebut="allo"
-      dateDeFin="allo"
-      informationsSupplémentairesText="allo"
-      :isPageDetaillerDemandeDeStage="true"
-      :isPageDetaillerDemandeOffre="false"
-      :isPageDetaillerCandidat="false">
-      <modalSuppression
-          :etatDuModal="true" 
-          @suppressionAnnuleeIcone="annulerSuppressionIcone"
-          @suppressionAnnulee="annulerSuppression"
-          @confirmationSuppression="suppressionConfirmee"
-          title="title"
-          firstName="firstName"
-          lastName="lastName"
-      />
-      </ficheDetailler>
+  <RouterLink to="/app/formulaireDo" class="bg-fuchsia-800 hover:bg-fuchsia-900 text-white text-sm font-bold p-4 rounded-xl mb-16">
+    <i class="ficheDetaillee__icône-consulter fa fa-check"></i>
+  </RouterLink>
+  <RouterLink to="/app/formulaireDo" class="bg-fuchsia-800 hover:bg-fuchsia-900 text-white text-sm font-bold p-4 rounded-xl mb-16">
+    <i class="ficheDetaillee__icône-editer fas fa-edit"></i>
+  </RouterLink>
+  <ficheDetailler
+    :isPageDetaillerDemandeDeStage="true"
+    :isPageDetaillerDemandeOffre="false"
+    :isPageDetaillerCandidat="false">
+  </ficheDetailler>
 </template>
 
 <script setup>
 import ficheDetailler from '@/components/ficheDetailler.vue';
-import modalSuppression from '@/components/modalSuppression.vue';
-import { ref } from 'vue'
-
-const etatDuModal = ref(false)
-
-const annulerSuppressionIcone = () => {
-  etatDuModal.value = false;
-  console.log(etatDuModal.value)
-}
-
-const annulerSuppression = () => {
-  etatDuModal.value = false;
-  console.log(etatDuModal.value)
-}
-
-const suppressionConfirmee = () => {
-  etatDuModal.value = false;
-  // Information de lapi pour la suppression
-  console.log(etatDuModal.value)
-}
-
-const toggleModal = () => {
-  etatDuModal.value = !etatDuModal.value;
-}
 </script>
