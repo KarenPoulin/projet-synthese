be9b40cd7403bf7303a51a42b6fbfcd19a5fb8be
Megan Martel <115752040+meganmartel@users.noreply.github.com>
2024-03-21 21:24:15 -0400

    enlever les commentaires

--- src/views/Entreprises.vue
+++ src/views/Entreprises.vue
@@ -3,9 +3,6 @@
       <div>
         <RouterLink to="/app/formulaireCandidat" class="bg-blue-400 hover:bg-blue-500 hover:text-white text-gray-600 text-sm font-bold p-4 rounded-xl mb-16">Ajouter une entreprise</RouterLink>
         <div class="grid grid-cols-3 grid-rows-4 gap-12 mt-16">
-<!--           <carteCandidatEtEntreprise :isEntreprise="true"/>
-          <carteCandidatEtEntreprise :isEntreprise="true"/>
-          <carteCandidatEtEntreprise :isEntreprise="true"/> -->
           <template v-if="allEnterprisesResults" v-for="enterprise in allEnterprisesResults" :key="enterprise._id">
             <RouterLink :to="{name:'fichedetailentreprise', params:{id: enterprise._id}}">
                 <carteCandidatEtEntreprise :enterprise="enterprise" :isEntreprise="true"/>
