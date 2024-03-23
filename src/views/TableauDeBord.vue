50fa06d86c6accf7800cc27089d97df549156f8e
karen102_14 <karen102_14@hotmail.com>
2024-03-23 19:37:14 -0400

    injection des tableaux dans le tableau de bord

--- src/views/TableauDeBord.vue
+++ src/views/TableauDeBord.vue
@@ -1,25 +1,44 @@
 <template>
-<!--     <table class="w-full mb-10">
-        <EnteteTableau :isDemandes="true" :isTableauDeBord="true" />
-        <ElementTableau :isDemandes="true" :isTableauDeBord="true" :isActive="true" />
-    </table>
-    <table class="w-full mb-10">
-        <EnteteTableau :isDemandes="false" :isTableauDeBord="true" />
-        <ElementTableau :isDemandes="false" :isTableauDeBord="true" :isActive="true"/>
-    </table>
-    <table class="w-full mb-10">
-        <EnteteTableau :isDemandes="true" :isTableauDeBord="false"  />
-        <ElementTableau :isDemandes="true" :isTableauDeBord="false" :isActive="true"/>
-        <ElementTableau :isDemandes="true" :isTableauDeBord="false" :isActive="false"/>
-    </table>
-    <table class="w-full mb-10">
-        <EnteteTableau :isDemandes="false" :isTableauDeBord="false" />
-        <ElementTableau :isDemandes="false" :isTableauDeBord="false" :isActive="true"/>
-        <ElementTableau :isDemandes="false" :isTableauDeBord="false" :isActive="false"/>
-    </table> -->
+
+    <div v-if="allDemandeDeStagesResults" class="mt-4 mb-16 px-10 py-16 bg-white rounded-lg">
+        <div class="mb-16">
+            <h2 class="text-lg font-bold">Dernières <span class="text-yellow-600">demandes</span> de stage</h2>
+            <p class="text-sm text-red-700">En attente de validation</p>
+        </div>
+        <table class="w-full mb-10">
+            <EnteteTableau :isDemandes="true" :isTableauDeBord="true" />
+            <template v-for="demandeDeStage in allDemandeDeStagesResults" :key="demandeDeStage._id">
+                <ElementTableau :element="demandeDeStage" :isDemandes="true" :isTableauDeBord="true" />
+            </template>
+        </table>
+    </div>
+
+    <div v-if="allOffreDeStagesResults" class="mt-4 px-10 py-16 bg-white rounded-lg">
+        <div class="mb-16">
+            <h2 class="text-lg font-bold">Dernières <span class="text-red-700">offres</span> de stage</h2>
+            <p class="text-sm text-red-700">En attente de validation</p>
+        </div>
+        <table class="w-full mb-10">
+            <EnteteTableau :isDemandes="false" :isTableauDeBord="true" />
+            <template v-for="offreDeStage in allOffreDeStagesResults" :key="offreDeStage._id">
+                <ElementTableau :element="offreDeStage" :isDemandes="false" :isTableauDeBord="true" />
+            </template>
+        </table>
+    </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
+import { useAllDemandeDeStages } from '@/composables/demandeDeStage';
+import { useAllOffreDeStages } from '@/composables/offreDeStage';
 import ElementTableau from '../components/elementTableau.vue'
 import EnteteTableau from '../components/enteteTableau.vue'
+
+const { allDemandeDeStagesResults, getAllDemandeDeStages } = useAllDemandeDeStages();
+const { allOffreDeStagesResults, getAllOffreDeStages } = useAllOffreDeStages();
+
+onMounted(async () => {
+    await getAllDemandeDeStages();
+    await getAllOffreDeStages();
+})
 </script>
