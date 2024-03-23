f06bcbd985761d9a336e4d3847b3269297f7035c
karen102_14 <karen102_14@hotmail.com>
2024-03-23 18:50:21 -0400

    Injections des offres de stage dans la route des offres de stage

--- src/views/OffresDeStages.vue
+++ src/views/OffresDeStages.vue
@@ -5,18 +5,39 @@
       une offre</button>
   </div>
 
+  <div class="mt-4 px-10 py-16 bg-white rounded-lg">
+    <table v-if="allOffreDeStagesResults" class="w-full mb-10">
+        <EnteteTableau :isDemandes="false" :isTableauDeBord="false" />
+        <template v-for="offreDeStage in allOffreDeStagesResults" :key="offreDeStage._id">
+            <ElementTableau :element="offreDeStage" :isDemandes="false" :isTableauDeBord="false" />
+        </template>
+    </table>
+  </div>
 
+<!--   <TableauDemandesEtOffres :isDemandes="false" :isTableauDeBord="false"/> -->
 
 
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router'
+import { useAllOffreDeStages } from '@/composables/offreDeStage';
+import ElementTableau from '../components/elementTableau.vue'
+import EnteteTableau from '../components/enteteTableau.vue'
+import TableauDemandesEtOffres from '../components/tableauDemandesEtOffres.vue'
 
 // Fonction pour émettre les routes dynamiques vers le formulaireDO pour une offre
 const router = useRouter()
 const goToFormDO = type => {
   router.push({ name: 'formulairedo', params: { type } })
 }
+
+const { allOffreDeStagesResults, getAllOffreDeStages } = useAllOffreDeStages();
+
+
+onMounted(async () => {
+  await getAllOffreDeStages();
+})
 
 </script>
