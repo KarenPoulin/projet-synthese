a6db201a8e32b5178c32fe313a546fa48c9435c4
Natissa-DidyToure <natissadidy@gmail.com>
2024-03-23 16:58:50 -0400

    modif sidebar

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -10,7 +10,7 @@
             <!--Bouton 1-->
             <li class="pt-2">
                 <router-link :to="{ name: 'tableaudebord' }"
-                    class="flex flex-col xl:flex-row items-center justify-center p-4 transition-all duration-300 text-teal-500 router-link"
+                    class="flex flex-col xl:flex-row items-center p-4 transition-all duration-300 text-teal-500 router-link"
                     active-class="active">
                     <img src="../assets/icons/sidebar-danshboard-24.png" alt="Icon 1" class="h-6 w-6 xl:mr-8">
                     <!-- Icône du bouton -->
@@ -22,7 +22,7 @@
             <!--Bouton 2-->
             <li class="pt-2">
                 <router-link :to="{ name: 'demandesdestages' }"
-                    class="flex flex-col xl:flex-row items-center justify-center p-4 transition-all duration-300 text-yellow-600 router-link"
+                    class="flex flex-col xl:flex-row items-center p-4 transition-all duration-300 text-yellow-600 router-link"
                     active-class="active">
                     <img src="../assets/icons/sidebar-request-24.png" alt="Icon 2" class="h-6 w-6 xl:mr-8">
                     <!-- Icône du bouton -->
@@ -34,7 +34,7 @@
             <!--Bouton 3-->
             <li class="pt-2">
                 <router-link :to="{ name: 'offresdestages' }"
-                    class="flex flex-col xl:flex-row items-center justify-center p-4 transition-all duration-300 text-red-800 router-link"
+                    class="flex flex-col xl:flex-row items-center p-4 transition-all duration-300 text-red-800 router-link"
                     active-class="active">
                     <img src="../assets/icons/sidebar-offer.png" alt="Icon 3" class="h-6 w-6 xl:mr-8">
                     <!-- Icône du bouton -->
@@ -46,7 +46,7 @@
             <!--Bouton 4-->
             <li class="pt-2">
                 <router-link :to="{ name: 'candidats' }"
-                    class="flex flex-col xl:flex-row items-center justify-center p-4 transition-all duration-300 text-fuchsia-800 router-link"
+                    class="flex flex-col xl:flex-row items-center p-4 transition-all duration-300 text-fuchsia-800 router-link"
                     active-class="active">
                     <img src="../assets/icons/sidebar-candidate-24.png" alt="Icon 4" class="h-6 w-6 xl:mr-8">
                     <!-- Icône du bouton -->
@@ -58,7 +58,7 @@
             <!--Bouton 5-->
             <li class="pt-2">
                 <router-link :to="{ name: 'entreprises' }"
-                    class="flex flex-col xl:flex-row items-center justify-center p-4 transition-all duration-300 text-blue-400 router-link"
+                    class="flex flex-col xl:flex-row items-center p-4 transition-all duration-300 text-blue-400 router-link"
                     active-class="active">
                     <img src="../assets/icons/sidebar-enterprise-24.png" alt="Icon 5" class="h-6 w-6 xl:mr-8">
                     <!-- Icône du bouton -->
