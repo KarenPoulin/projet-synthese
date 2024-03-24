b018a7dbeb9b4ed5cca55c8e858282bcfb542305
Jonathan Claes <115749108+JoMesss123@users.noreply.github.com>
2024-03-24 08:35:11 -0400

    combinaison des deux formulaire

--- src/views/Candidats.vue
+++ src/views/Candidats.vue
@@ -1,7 +1,7 @@
 <template>
       <h1 class="text-5xl font-bold mb-20 text-neutral-500">Candidats</h1>
       <div>
-        <RouterLink to="/app/formulaireCandidat" class="bg-fuchsia-800 hover:bg-fuchsia-900 text-white text-sm font-bold p-4 rounded-xl mb-16">Ajouter un candidat</RouterLink>
+        <RouterLink to="{name: 'formulaireCE', params: {type: 'candidat'}}" class="bg-fuchsia-800 hover:bg-fuchsia-900 text-white text-sm font-bold p-4 rounded-xl mb-16">Ajouter un candidat</RouterLink>
         <div class="grid grid-cols-1 lg:grid-cols-4 lg:grid-rows-4 gap-10 mt-16">
           <template v-if="allCandidatesResults" v-for="candidat in allCandidatesResults" :key="candidat._id">
               <RouterLink :to="{name:'fichedetailcandidat', params:{id: candidat._id}}">
