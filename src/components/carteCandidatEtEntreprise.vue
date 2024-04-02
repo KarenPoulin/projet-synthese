4a8a0026f9e9929a1a27f0f114f8b5cfd5991821
karen102_14 <karen102_14@hotmail.com>
2024-04-02 19:04:20 -0400

    Ajustement des tailles image candidats au responsive

--- src/components/carteCandidatEtEntreprise.vue
+++ src/components/carteCandidatEtEntreprise.vue
@@ -1,10 +1,10 @@
 <template>
-    <div v-if="isCandidat" class="bg-white p-5 hover:bg-fuchsia-100 ">
+    <div v-if="isCandidat" class="bg-white p-5 w-84 shrink hover:bg-fuchsia-100 ">
         <div class="flex items-center">
-            <div class="bg-fuchsia-800 p-2 flex justify-center items-center">
-                <img src="/src/assets/icons/candidate-48.png" alt="candidat" class="w-20">
+            <div class="bg-fuchsia-800 p-1.5 w-20 h-20 flex justify-center items-center">
+                <img src="/src/assets/icons/candidate-48.png" alt="candidat" class="w-14">
             </div>
-            <div class="ml-3">
+            <div class="ml-3 w-2/3">
                 <h1 class="text-md lg:text-lg font-bold">{{candidate.firstName}} {{ candidate.lastName }}</h1>
                 <p class="text-xs mb-3 text-neutral-500">Développeur Front-End</p>
                 <p class="text-xs text-neutral-500">{{ candidate.email }}</p>
