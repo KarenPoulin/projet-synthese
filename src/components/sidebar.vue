9007674b3267ef000995ada58db1313b2c2499dc
Natissa-DidyToure <natissadidy@gmail.com>
2024-04-03 13:46:13 -0400

    Ajout de la logique pour que la bordure reste active sur les bonne sections en mode formulaire et fiche de détail

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -21,21 +21,22 @@
             </li>
             <!--Bouton 2-->
             <li class="pt-2">
-                <router-link :to="{ name: 'demandesdestages' }"
-                    class="flex flex-col lg:flex-row items-center px-0 sm:px-4 py-2 xl:py-4 transition-all duration-300 text-yellow-600 router-link"
-                    active-class="active">
-                    <img src="../assets/icons/sidebar-request-24.png" alt="Icon 2" class="h-4 w-4 sm:h-6 sm:w-6 lg:mr-4 xl:mr-8 object-contain">
-                    <!-- Icône du bouton -->
-                    <span class="link-text hidden lg:block text-xs xl:text-sm">Demandes de stages</span>
-                    <span class="indicator w-0 h-full bg-transparent transition-all duration-300"></span>
-                    <!-- Effet de survol -->
-                </router-link>
-            </li>
+        <router-link :to="{ name: 'demandesdestages' }"
+            class="flex flex-col lg:flex-row items-center px-0 sm:px-4 py-2 xl:py-4 transition-all duration-300 text-yellow-600 router-link"
+            active-class="active"
+            :class="{ 'active': isDemandeStageActive }">
+            <img src="../assets/icons/sidebar-request-24.png" alt="Demandes de stages" class="h-4 w-4 sm:h-6 sm:w-6 lg:mr-4 xl:mr-8 object-contain">
+            <span class="link-text hidden lg:block text-xs xl:text-sm">Demandes de stages</span>
+            <span class="indicator w-0 h-full bg-transparent transition-all duration-300"></span>
+        </router-link>
+    </li>
+
             <!--Bouton 3-->
             <li class="pt-2">
                 <router-link :to="{ name: 'offresdestages' }"
                     class="flex flex-col lg:flex-row items-center px-0 sm:px-4 py-2 xl:py-4 transition-all duration-300 text-red-800 router-link"
-                    active-class="active">
+                    active-class="active"
+                    :class="{ 'active': isOffreStageActive }">
                     <img src="../assets/icons/sidebar-offer.png" alt="Icon 3" class="h-4 w-4 sm:h-6 sm:w-6 lg:mr-4 xl:mr-8 object-contain">
                     <!-- Icône du bouton -->
                     <span class="link-text hidden lg:block text-xs xl:text-sm">Offres de stages</span>
@@ -47,7 +48,8 @@
             <li class="pt-2">
                 <router-link :to="{ name: 'candidats' }"
                     class="flex flex-col lg:flex-row items-center px-0 sm:px-4 py-2 xl:py-4 transition-all duration-300 text-fuchsia-800 router-link"
-                    active-class="active">
+                    active-class="active"
+                    :class="{ 'active': isCandidatActive }">
                     <img src="../assets/icons/sidebar-candidate-24.png" alt="Icon 4" class="h-4 w-4 sm:h-6 sm:w-6 lg:mr-4 xl:mr-8 object-contain">
                     <!-- Icône du bouton -->
                     <span class="link-text hidden lg:block text-xs xl:text-sm">Candidats</span>
@@ -59,7 +61,8 @@
             <li class="pt-2">
                 <router-link :to="{ name: 'entreprises' }"
                     class="flex flex-col lg:flex-row items-center px-0 sm:px-4 py-2 xl:py-4 transition-all duration-300 text-blue-400 router-link"
-                    active-class="active">
+                    active-class="active"
+                    :class="{ 'active': isEntrepriseActive }">
                     <img src="../assets/icons/sidebar-enterprise-24.png" alt="Icon 5" class="h-4 w-4 sm:h-6 sm:w-6 lg:mr-4 xl:mr-8 object-contain">
                     <!-- Icône du bouton -->
                     <span class="link-text hidden lg:block text-xs xl:text-sm">Entreprises</span>
@@ -74,6 +77,37 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'; 
+import { useRoute } from 'vue-router'; 
+
+
+const route = useRoute();
+
+// Logique pour garder la route pour ajouter demandes de stages active
+const isDemandeStageActive = computed(() => {
+
+  return route.name === 'formulairedo' && route.params.type === 'request';
+});
+
+//Logique pour garder la route pour ajouter offres de stages active
+const isOffreStageActive = computed(() => {
+
+  return route.name === 'formulairedo' && route.params.type === 'offer';
+});
+
+//Logique pour garder la route pour ajouter entreprises active
+const isEntrepriseActive = computed(() => {
+
+  return route.name === 'formulaireCE' && route.params.type === 'entreprises';
+});
+
+//Logique pour garder la route pour ajouter candidats active
+const isCandidatActive = computed(() => {
+
+  return route.name === 'formulaireCE' && route.params.type === 'candidat';
+});
+
+
 
 </script>
 
@@ -92,4 +126,5 @@
 .active {
     border-right: 4px solid;
 }
+
 </style>
